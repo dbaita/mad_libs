a5e52880782f0ae6855767989cd1547a9e77923c
dbaita <gbelittle@gmail.com>
2018-01-22 18:25:04 -0500

    Add files via upload

---
+++ mad_libs.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 22 13:31:27 2018
+
+@author: dalton
+"""
+
+food = input('Enter a food:') + ','
+adj1 = input('Enter an adjective:')
+adj2 = input('Enter another adjective:')
+noun = input('Enter a noun:') + '.'
+verb = input('Enter a verb present ending in ING:') + ','
+noun2 = input('Enter a noun plural:') + '.'
+noun3 = input('Enter another noun plural:') + '.'
+
+
+print()
+print()
+print()
+
+
+print('With a new beginning and fresh piece of', food)
+print('Full of inspiration and', adj1, 'thoughts,')
+print("Let's begin this year with a", adj2, noun)
+print('Gone are the days of regret and', verb)
+print('These rooms full of', noun2)
+print("It's time to move with courage,")
+print('Full of confidence and', noun3)
+print("Let's begin this year with a", adj2, noun)
+
